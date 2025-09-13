144334ca567b09c536ff7d3e5e533ca44934b4b0
ygyesus <yosefgyesus.in@gmail.com>
2025-09-13 15:08:21 +0300

    commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import SignInForm from '../components/SignInForm';
 
 export default function LoginPage() {
-    return <SignInForm />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <SignInForm />
+        </Suspense>
+    );
 }
